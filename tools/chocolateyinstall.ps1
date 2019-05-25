2b98c73d246630fe1d574a4eb943e4708ac9fc91
ripclawffb <ripclaw_ffb@hotmail.com>
2019-05-25 14:43:35 -0700

    Initial release

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,62 @@
+﻿
+$ErrorActionPreference = 'Stop';
+
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$androidPath = "${Env:SystemDrive}\Android"
+$destination = "${androidPath}"
+$url        = 'https://dl.google.com/android/repository/android-ndk-r19c-windows-x86.zip'
+$url64      = 'https://dl.google.com/android/repository/android-ndk-r19c-windows-x86_64.zip'
+
+If(Get-OSArchitectureWidth -Compare 32) {
+  $zipFileName = $url.split('/')[-1]
+} Else {
+  $zipFileName = $url64.split('/')[-1]
+}
+
+If(Get-OSArchitectureWidth -Compare 32) {
+  $folderName = $url.split('/')[-1].replace('-windows-x86.zip','')
+} Else {
+  $folderName = $url64.split('/')[-1].replace('-windows-x86_64.zip','')
+}
+
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $destination
+  url           = $url
+  url64bit      = $url64
+
+  softwareName  = 'android-ndk*'
+
+  checksum      = '800C3C6BA616DDF25097D43566D5D574F9E6C0A10538BF60DD5BE0E024F732CD'
+  checksumType  = 'sha256'
+  checksum64    = '0FAF708C9837A921CAE5262745F5857162614BB9689A0D188780D12EA93A2C18'
+  checksumType64= 'sha256'
+}
+
+Install-ChocolateyZipPackage @packageArgs
+
+$packagelibPath = $env:ChocolateyPackageFolder
+
+Rename-Item -Path "$destination\$folderName" -NewName "$destination\$($packageArgs.softwareName.replace('*',''))"
+((Get-Content -path "$packagelibPath\$($zipFileName).txt" -Raw) -replace "$($destination.replace("\","\\"))\\$folderName","$destination\$($packageArgs.softwareName.replace('*',''))") | Set-Content -Path "$packagelibPath\$($zipFileName).txt"
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
---
+++ update.ps1
@@ -0,0 +1,43 @@
+import-module au
+
+$releases = 'https://developer.android.com/ndk/downloads'
+
+function global:au_SearchReplace {
+   @{
+        ".\tools\chocolateyInstall.ps1" = @{
+            "(?i)(^[$]packageName\s*=\s*)('.*')"  = "`$1'$($Latest.PackageName)'"
+            "(?i)(^[$]softwareName\s*=\s*)('.*')" = "`$1'$($Latest.PackageName)*'"
+            "(?i)(^[$]url\s*=\s*)('.*')"          = "`$1'$($Latest.URL32)'"
+            "(checksum\s*=\s*)('.*')"             = "`$1'$($Latest.Checksum32)'"
+            "(?i)(^[$]url64\s*=\s*)('.*')"        = "`$1'$($Latest.URL64)'"
+            "(checksum64\s*=\s*)('.*')"           = "`$1'$($Latest.Checksum64)'"
+        }
+
+        "$($Latest.PackageName).nuspec" = @{
+            "(\<releaseNotes\>).*?(\</releaseNotes\>)" = "`${1}$($Latest.ReleaseNotes)`$2"
+        }
+    }
+}
+
+function global:au_GetLatest {
+    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+
+    $match = $download_page.Content | Select-String -Pattern '(android-ndk-.*-windows.*\.zip)'
+    $url = $match.Matches[0].value
+    
+    $version  = $url -split '[_-]|.zip' | Select-Object -Last 1 -Skip 3
+
+    @{
+        Version      = $version
+        URL32        = $url -replace 'amd64','i386'
+        URL64        = $url
+        ReleaseNotes = 'https://github.com/influxdata/telegraf/blob/master/CHANGELOG.md'
+    }
+}
+
+try {
+    update -ChecksumFor all
+} catch {
+    $ignore = 'Unable to connect to the remote server'
+    if ($_ -match $ignore) { Write-Host $ignore; 'ignore' }  else { throw $_ }
+}
